b810bb36c10852ee4f55b9c2ae4ff8b880c13d66
Jutmzg <jul.bousseau@gmail.com>
2019-07-18 11:57:53 +0200

    scss

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Artist;
 use App\Entity\Event;
+use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,9 +21,14 @@ class EventType extends AbstractType
             ->add('place')
             ->add('date')
             ->add('img')
-            ->add('artist')
-            //'choice_label' => function ($artist){return $artist->}])
-            ->add('user')
+            ->add('artist', EntityType::class, [
+                'class' => Artist::class,
+                'choice_label' => 'name'
+                ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'email'
+            ])
         ;
     }
 
